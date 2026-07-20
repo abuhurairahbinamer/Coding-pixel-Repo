3e11f466e8707a39330d83c395a8c379ac4fd68d
abuhurairahbinamer <dkspklhrd007@gmail.com>
2026-07-20 22:17:13 +0500

    feat:Applied deeper in practice question P8

--- Assignemnt1/Assignment1/practice/p8.mjs
+++ Assignemnt1/Assignment1/practice/p8.mjs
@@ -14,3 +14,13 @@ const multiply2 = (x) => x * 2;
 const result = pipe(add1, multiply2)(3);
 
 console.log(result); 
+//deeper
+const deeperpipe = (...fns) => {
+  return (x) => {
+    return fns.reduceRight((value, fn) => {
+      return fn(value);
+    }, x);
+  };
+};
+const resultRight = deeperpipe(add1, multiply2)(3);
+console.log(resultRight)
